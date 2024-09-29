e47bd79bd321e1d19e45e02056fb8f550f02dbb7
nabila ahmed <sumayaad2020@gmail.com>
2024-09-29 18:19:20 +0600

    first commit

---
+++ sets.py
@@ -0,0 +1,16 @@
+myset ={1,2,3,3,3,2,2}
+print(myset)
+
+myset.add("Nabila")
+myset.add("Rose")
+myset.add("Nusaiba")
+print(myset)
+
+myset.pop()
+myset.pop()
+print(myset)
+
+
+l1 =["x","y","z","x"]
+newset = set(l1)
+print(newset)
