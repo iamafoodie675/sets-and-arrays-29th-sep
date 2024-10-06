a4420ea1dd48f4f5184df59fd98026322b99a745
nabila ahmed <sumayaad2020@gmail.com>
2024-10-06 15:23:37 +0600

    abc

---
+++ set_symmetric_difference.py
@@ -0,0 +1,14 @@
+
+#A
+set1_A = {'blue', 'green'}
+set2_A = {'blue', 'yellow'}
+
+symmetric_difference1= set1_A.symmetric_difference(set2_A)
+print(symmetric_difference1)
+
+#B
+set1_B = {1,2,3,4,5}
+set2_B = {1,5,6,7,8,9}
+
+symmetric_difference2 = set1_B.symmetric_difference(set2_B)
+print(symmetric_difference2)
